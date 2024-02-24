076de9f646c299335170c695048ca3c06bcab1c3
raiideluxe <artemreshetnikov.one@gmail.com>
2024-02-24 13:29:59 +0400

    Second commit

--- task1.py
+++ task1.py
@@ -1,3 +1,6 @@
 room = int(input('Номер квартиры: '))
 entrance = (room-1)//8+1
-print(entrance)
+floor = (room-1)%8+1
+print('Подъезд: ',entrance)
+print('Этаж:',floor)
+
---
+++ task2.py
@@ -0,0 +1,4 @@
+first_number = int(input('Первое слагаемое: '))
+second_number = int(input('Второе слагаемое: '))
+result = first_number+second_number
+print('Результат: ',result)
