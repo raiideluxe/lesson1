e84b463c9e025bf4135c89ef01dbc0542b20d628
raiideluxe <artemreshetnikov.one@gmail.com>
2024-02-24 14:44:44 +0400

    Second commit

--- task18.py
+++ task18.py
@@ -1,14 +1,2 @@
-def is_point_in_circle(x,y):
-    return (x-1)**2 + y**2 <= 2**2 and (x-1)**2 + y**2>=1**2
-def is_point_in_rectangle(x,y):
-    return abs(x-4)<2 and abs(y-2)<3
-x = int(input('x:'))
-y = int(input('y:'))
-if is_point_in_circle(x,y) and is_point_in_rectangle(x,y):
-    print('yes yes')
-if is_point_in_circle(x,y):
-    print('yes no')
-if is_point_in_rectangle(x,y):
-    print('no yes')
-else:
-    print('no no')
+for i in range(3, 11, 4):
+    print(i)
--- task19.py
+++ task19.py
@@ -1,14 +1,9 @@
-def is_point_in_circle(x,y):
-    return (x-1)**2 + y**2 <= 2**2 and (x-1)**2 + y**2>=1**2
-def is_point_in_rectangle(x,y):
-    return abs(x-4)<2 and abs(y-2)<3
-x = int(input('x:'))
-y = int(input('y:'))
-if is_point_in_circle(x,y) and is_point_in_rectangle(x,y):
-    print('yes yes')
-if is_point_in_circle(x,y):
-    print('yes no')
-if is_point_in_rectangle(x,y):
-    print('no yes')
-else:
-    print('no no')
+list = [2, 6, 43, 1, 66]
+s=0
+for item in list:
+    if item % 2 == 0:
+        s += 1
+    else:
+        continue
+
+print(s)
